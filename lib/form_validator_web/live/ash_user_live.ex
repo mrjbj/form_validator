65879b4e6a390f9c5500c0b393b124ac22c33f3d
Jason Jones <jason@brucejones.biz>
2022-07-07 16:51:42 -0400

    initial take on AshPhoenix.Form

--- lib/form_validator_web/live/ash_user_live.ex
+++ lib/form_validator_web/live/ash_user_live.ex
@@ -59,31 +59,25 @@ defmodule FormValidatorWeb.AshUserLive do
     ~H"""
     <section class="mt-24 w-1/2 shadow flex flex-col items-center justify-center mx-auto p-6 bg-white">
       <h1 class="text-4xl font-bold italic text-gray-700">
-       User and Tweets
+        User and Tweets
       </h1>
       <p class="text-gray-500 font-semibold text-lg mt-6 text-center px-8">
         Showing Users and Tweets for James
       </p>
 
-
-
-
       <.form let={f} for={@form} phx-change="validate" phx-submit="save" id="form_assign_id">
-
-        <%= for tweet_form <- inputs_for(f, :tweets) do  %>
-            <%= text_input tweet_form, :body %>
+        <%= for tweet_form <- inputs_for(f, :tweets) do %>
+          <%= text_input(tweet_form, :body, class: "w-full py-2") %>
         <% end %>
 
         <div>
-          <%= submit "Save",
+          <%= submit("Save",
             phx_disable_with: "Saving...",
-            class: "w-full py-2 border-none shadow rounded font-semibold text-sm text-gray-50 hover:bg-violet-600 bg-violet-500"
-           %>
+            class:
+              "w-full py-2 border-none shadow rounded font-semibold text-sm text-gray-50 hover:bg-violet-600 bg-violet-500"
+          ) %>
         </div>
       </.form>
-
-
-
 
       <p class="text-sm px-10 text-center mt-6 text-gray-400 font-semibold">
         By signing up, you agree to Terms, Data and Cookies Policy.
