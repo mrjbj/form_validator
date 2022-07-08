3a72b19ed428c51cdda82c6c931e88f5ec484e32
Jason Jones <jason@brucejones.biz>
2022-07-07 21:14:50 -0400

    got table working...

--- lib/form_validator_web/components/tweet_component.ex
+++ lib/form_validator_web/components/tweet_component.ex
@@ -1,29 +1,32 @@
 defmodule FormValidatorWeb.TweetComponent do
   use Phoenix.Component
 
-           # <p>This is a Tweet. The message is <%= @body %>.</p>
+  # <p>This is a Tweet. The message is <%= @body %>.</p>
   def tweet(assigns) do
     ~H"""
-       <div>
-
-                  <tr>
-                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                      Lindsay Walton
-                    </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      Front-end Developer
-                    </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      lindsay.walton@example.com
-                    </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
-                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                      <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                        Edit<span class="sr-only">, Lindsay Walton</span>
-                      </a>
-                    </td>
-                  </tr>
-       </div>
+    <div>
+     <table class="min-w-full divide-y divide-gray-300">
+       <tbody class="divide-y divide-gray-200 bg-white">
+       <tr>
+        <td class="whitespace-nowrap py-3.5 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+          Lindsay Walton
+        </td>
+        <td class="whitespace-nowrap px-2 py-3.5 text-sm text-gray-500">
+          Front-end Developer
+        </td>
+        <td class="whitespace-nowrap px-2 py-3.5 text-sm text-gray-500">
+          lindsay.walton@example.com
+        </td>
+        <td class="whitespace-nowrap px-2 py-3.5 text-sm text-gray-500">Member</td>
+        <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+          <a href="#" class="text-indigo-600 hover:text-indigo-900">
+            Edit<span class="sr-only">, Lindsay Walton</span>
+          </a>
+        </td>
+      </tr>
+      </tbody>
+     </table>
+    </div>
     """
   end
 end
--- lib/form_validator_web/live/ash_user_live.ex
+++ lib/form_validator_web/live/ash_user_live.ex
@@ -3,6 +3,7 @@ defmodule FormValidatorWeb.AshUserLive do
 
   import Ash.Query
   import FormValidatorWeb.TweetComponent
+  import FormValidatorWeb.TweetHeader
 
   alias Ash.Query
   alias AshPhoenix.Form
@@ -58,91 +59,117 @@ defmodule FormValidatorWeb.AshUserLive do
   #          <%= text_input(tweet_form, :body, class: "w-full py-2") %>
   # end
 
+  #         <.form let={f} for={@form} phx-change="validate" phx-submit="save" id="form_assign_id">
+  #           <%= for tweet_form <- inputs_for(f, :tweets) do %>
+  #             <.tweet tweet={tweet_form} />
+  #           <% end %>
+  #
+  #           <%= submit("Save",
+  #             phx_disable_with: "Saving...",
+  #             class:
+  #               "w-full py-2 border-none shadow rounded font-semibold text-sm text-gray-50 hover:bg-violet-600 bg-violet-500"
+  #           ) %>
+  #         </.form>
+
   def render(assigns) do
     ~H"""
-    <div class="px-4 sm:px-6 lg:px-8">
-      <div class="sm:flex sm:items-center">
-        <div class="sm:flex-auto">
-          <h1 class="text-xl font-semibold text-gray-900">Users</h1>
-          <p class="mt-2 text-sm text-gray-700">
-            A list of all of your tweets!
-          </p>
+    <.form let={f} for={@form} phx-change="validate" phx-submit="save" id="form_assign_id">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+          <div class="sm:flex-auto">
+            <h1 class="text-xl font-semibold text-gray-900">Transactions</h1>
+            <p class="mt-2 text-sm text-gray-700">
+              A table of placeholder stock market data that does not make any sense.
+            </p>
+          </div>
+          <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <button
+              type="button"
+              class="inline-flex items-center justify-center rounded-md border border-transparent bg-primary-600 px-8 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+            >
+              Export
+            </button>
+          </div>
         </div>
-        <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-          >
-            Add tweet
-          </button>
-        </div>
-      </div>
-      <div class="mt-8 flex flex-col">
-        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-300">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                    >
-                     ID
-                    </th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                     Tweet
-                    </th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                     Public
-                    </th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                     Created
-                    </th>
-                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                      <span class="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200 bg-white">
-                  <!-- More people... -->
-                </tbody>
-              </table>
+        <div class="mt-8 flex flex-col">
+          <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-t-lg">
+                <table class="min-w-full divide-y divide-gray-300">
+                  <thead class="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                      >
+                        Transaction ID
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Company
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Share
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Commision
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Price
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Quantity
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Net amount
+                      </th>
+                      <th scope="col" class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6">
+                        <span class="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-200 bg-white">
+                    <%= for tweet_form <- inputs_for(f, :tweets) do %>
+                      <.tweet tweet={tweet_form} />
+                    <% end %>
+                    <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                      <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                        Edit<span class="sr-only">, AAPS0L</span>
+                      </a>
+                    </td>
+                    <!-- More transactions... -->
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-
-    <section class="mt-24 w-1/2 shadow flex flex-col items-center justify-center mx-auto p-6 bg-white">
-      <h1 class="text-4xl font-bold italic text-gray-700">
-        User and Tweets
-      </h1>
-      <p class="text-gray-500 font-semibold text-lg mt-6 text-center px-8">
-        Showing Users and Tweets for James
-      </p>
-
-      <.form let={f} for={@form} phx-change="validate" phx-submit="save" id="form_assign_id">
-        <%= for tweet_form <- inputs_for(f, :tweets) do %>
-           <.tweet body="Hello" />
-        <% end %>
-
-        <div>
+        <div class="mt-4 px-8 sm:mt-0 sm:ml-16 sm:flex-none flex justify-end">
           <%= submit("Save",
             phx_disable_with: "Saving...",
             class:
-              "w-full py-2 border-none shadow rounded font-semibold text-sm text-gray-50 hover:bg-violet-600 bg-violet-500"
+              "w-1/6 py-2 border-none shadow rounded font-semibold text-sm text-gray-50 hover:bg-primary-600 bg-primary-500"
           ) %>
         </div>
-      </.form>
-
-      <p class="text-sm px-10 text-center mt-6 text-gray-400 font-semibold">
-        By signing up, you agree to Terms, Data and Cookies Policy.
-      </p>
-      <pre>
-          Hi, this is a test area for "pre"
-          </pre>
-    </section>
+      </div>
+    </.form>
     """
   end
 end
